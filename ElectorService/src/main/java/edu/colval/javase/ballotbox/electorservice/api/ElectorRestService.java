b488ccd99bfa24f9b681158255e5bfb70d357166
Guilbault2063074 <81373503+Guilbault2063074@users.noreply.github.com>
2022-04-30 22:08:11 -0400

    Update ElectorRestService.java

--- ElectorService/src/main/java/edu/colval/javase/ballotbox/electorservice/api/ElectorRestService.java
+++ ElectorService/src/main/java/edu/colval/javase/ballotbox/electorservice/api/ElectorRestService.java
@@ -1,8 +1,43 @@
 package edu.colval.javase.ballotbox.electorservice.api;
 
-import org.springframework.web.bind.annotation.RestController;
+import edu.colval.javase.ballotbox.electorservice.bll.model.Elector;
+import edu.colval.javase.ballotbox.electorservice.dal.IElectorDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+import java.util.List;
 
 @RestController
+@CrossOrigin
+@RequestMapping("/api/electorService")
 public class ElectorRestService {
+    @Autowired
+    private IElectorDAO electorDAO;
 
+    @PostMapping("/elector/create")
+    public Elector createElector(@RequestBody Elector newElector){
+        this.electorDAO.addElector(newElector);
+        return this.electorDAO.fetchElectorById(newElector.getId());
+    }
+
+    @GetMapping("/elector/findAllElectors")
+    public List<Elector> findAllElectors(){
+        return this.electorDAO.getAllElectors();
+    }
+
+    @GetMapping("/elector/findElector/{electorId}")
+    public Elector findElectorById(@PathVariable("electorId") int electorId) throws SQLException{
+        Elector foundElector = this.electorDAO.fetchElectorById(electorId);
+        return foundElector;
+    }
+
+
+
+    @DeleteMapping("/elector/delete/{electorId}")
+    public Elector deleteBallotById(@PathVariable("electorId") int electorId) throws SQLException {
+        Elector deletedElector = this.electorDAO.fetchElectorById(electorId);
+        this.electorDAO.deleteElector(electorId);
+        return deletedElector;
+    }
 }
